2b00290e9f765ebf0a355d6a8463ebad90b74a4d
Javier Irigoyen <jirigoyen@gmail.com>
2026-07-28 13:38:20 -0700

    auto-push: 20260728-v4.06-highlight-total-over-threshold

--- userscript.js
+++ userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         eBay Address Clipboard Copier and Printer (Radical UI Decoupled)
 // @namespace    http://tampermonkey.net/
-// @version      20260727-v4.05-select-non-label-count
+// @version      20260728-v4.06-highlight-total-over-threshold
 // @description  A nicer redesign of the eBay bulk shipping page with a polished, modern address box. Logic is now decoupled from configuration (templates/quotes) via external Gist.
 // @author       Javier, with modifications from Grok, Gemini, Claude, and GitHub Copilot <3
 // @match        https://gslblui.ebay.com/gslblui/bulk
